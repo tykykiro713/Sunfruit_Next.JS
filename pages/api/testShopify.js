7313471f244cd76a6b9fcb4637d89330a9011817
Tyson Rohde <tyson@sunfruit.co>
2025-01-30 10:09:57 -0500

    Updated project files and fixed deployment issues

--- pages/api/testShopify.js
+++ pages/api/testShopify.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const SHOPIFY_API_URL = process.env.NEXT_PUBLIC_SHOPIFY_API_URL;
-    const SHOPIFY_ACCESS_TOKEN = process.env.NEXT_PUBLIC_SHOPIFY_ACCESS_TOKEN;
+    const SHOPIFY_ACCESS_TOKEN = process.env.NEXT_SHOPIFY_ACCESS_TOKEN;
 
     const query = `
     {
